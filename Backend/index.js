6a19e4edbb077a0d29f19c030d2a5b39c1b65cb9
kavya <kavya02shyopura@gmail.com>
2024-07-26 11:58:10 +0530

    first commit

--- Backend/index.js
+++ Backend/index.js
@@ -2,7 +2,11 @@ import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import bookRoute from "./route/book.route.js"
+import cors from "cors"
+import userRoute from "./route/user.route.js"
 const app = express()
+app.use(cors());
+app.use(express.json());
 dotenv.config();
 
 const PORT = process.env.PORT || 4000
@@ -16,14 +20,11 @@ try {
     console.log("connected to mongodb")
 } catch (error) {
     console.log("error: ", error);
-
 }
 
 // defining routes
-app.use("/book", bookRoute)
-
-
-
+app.use("/book", bookRoute);
+app.use("/user", userRoute);
 
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`)
